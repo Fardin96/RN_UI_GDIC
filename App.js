8a536e2d553a33f075d1296b70d3bab26f5a19c6
Fardin96 <fardinshuvro96@gmail.com>
2024-01-24 20:01:21 +0600

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+
+import {StyleSheet, Text, View} from 'react-native';
+
+function App() {
+  return (
+    <View style={styles.root}>
+      <Text>hello, gdic</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet({
+  root: {flex: 1, alignItems: 'center', justifyContent: 'center'},
+});
+
+export default App;
